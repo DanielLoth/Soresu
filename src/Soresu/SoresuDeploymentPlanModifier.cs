3f7ce9f6f6e0b91068ef7bbce06cd7a0a67ecdef
Daniel Loth <lothyis@gmail.com>
2024-02-17 18:12:39 +1100

    Experimentation

--- src/Soresu/SoresuDeploymentPlanModifier.cs
+++ src/Soresu/SoresuDeploymentPlanModifier.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.SqlServer.Dac.Deployment;
 using Microsoft.SqlServer.Dac.Extensibility;
+using Microsoft.SqlServer.TransactSql.ScriptDom;
 
 namespace Soresu;
 
@@ -15,6 +16,68 @@ public class SoresuDeploymentPlanModifier : DeploymentPlanModifier
         //base.AddAfter(planHandle, lastStep, lastStep);
         //base.Remove(planHandle, lastStep);
 
-        throw new NotImplementedException();
+        var arguments = context.Arguments;
+        var options = context.Options;
+
+        var variables = context.Options.SqlCommandVariableValues;
+        
+        if (!variables.TryGetValue("Intent", out var intentAsString) ||
+            !Enum.TryParse<ReleaseIntent>(intentAsString, out var intent))
+        {
+            throw new InvalidOperationException("A value indicating intent must be specified, and it must be valid");
+        }
+
+        if (options.AllowTableRecreation && intent != ReleaseIntent.RecreateTable)
+        {
+            throw new InvalidOperationException("Cannot recreate table when release intent does not indicate this should happen");
+        }
+
+        var visitor = new Visitor();
+
+        var next = context.PlanHandle.Head;
+        while (next is not null)
+        {
+            if (next is DeploymentScriptDomStep domStep &&
+                domStep.Script is not null)
+            {
+                domStep.Script.Accept(visitor);
+            }
+
+            next = next.Next;
+        }
+
+        PublishMessage(new ExtensibilityError("Message", Severity.Message));
+
+        //throw new NotImplementedException();
     }
+}
+
+public class Visitor : TSqlConcreteFragmentVisitor
+{
+    public override void ExplicitVisit(AlterTableAlterColumnStatement node)
+    {
+        var existing = node.Options.OfType<OnlineIndexOption>().FirstOrDefault();
+
+        if (existing is not null)
+        {
+            existing.OptionState = OptionState.On;
+            return;
+        }
+
+        var option = new OnlineIndexOption
+        {
+            OptionKind = IndexOptionKind.Online,
+            OptionState = OptionState.On
+        };
+
+        node.Options.Add(option);
+    }
+}
+
+public enum ReleaseIntent
+{
+    CreateIndexOnly,
+    DropObjectOnly,
+    RecreateTable,
+    Standard
 }
--- test/Soresu.Test/UnitTest1.cs
+++ test/Soresu.Test/UnitTest1.cs
@@ -1,10 +1,136 @@
+using Microsoft.SqlServer.Dac;
+using Microsoft.SqlServer.Dac.Deployment;
+using Microsoft.SqlServer.Dac.Model;
+using Microsoft.SqlServer.TransactSql.ScriptDom;
+
 namespace Soresu.Test;
 
 public class UnitTest1
 {
-    [Fact]
-    public void Test1()
+    [Theory]
+    //[InlineData(
+    //    "create table A (Id int primary key);",
+    //    "create table A (Id int primary key, C1 int not null default 0);",
+    //    typeof(AlterTableAddTableElementStatement))]
+    [InlineData(
+        """
+        create table A (
+            Id int primary key,
+            C1 int not null
+                constraint CK1 check (C1=1)
+                constraint UQ1 unique
+                constraint DF1 default 0,
+            constraint AK1 unique (C1),
+            constraint AK1ToBeDropped unique (C1),
+            constraint TCK1 check (C1=1),
+            constraint TCK1ToBeDropped check (C1=1)
+        );
+        """,
+        """
+        create table A (
+            Id int primary key,
+            C1 bigint not null
+                constraint CK1 check (C1=1)
+                constraint UQ1 unique
+                constraint DF1 default 0,
+            constraint AK1 unique (C1),
+            constraint TCK1 check (C1=1)
+        );
+        """,
+        typeof(AlterTableAlterColumnStatement))]
+    public void Test1(string currentSql, string targetSql, Type type)
     {
+        using var sourcePackage = GetDacPackage(targetSql);
+        using var targetPackage = GetDacPackage(currentSql);
 
+        var dacDeployOptions = new DacDeployOptions
+        {
+            AllowTableRecreation = false
+        };
+
+        dacDeployOptions.SetVariable("Environment", "Production");
+        dacDeployOptions.SetVariable("Intent", ReleaseIntent.CreateIndexOnly.ToString());
+
+        var report = DacServices.GenerateDeployReport(sourcePackage, targetPackage, "MyDatabase", dacDeployOptions);
+        var script = DacServices.GenerateDeployScript(sourcePackage, targetPackage, "MyDatabase", dacDeployOptions);
+
+        var fragment = Parse(script);
+        var statements = GetStatements(script, type).FirstOrDefault();
+    }
+
+    [Fact]
+    public void Test2()
+    {
+        var sql = "alter table dbo.A alter column C1 int not null with (online = on);";
+        var fragment = Parse(sql);
+    }
+
+    private static TSqlModel GetTSqlModel(string sql)
+    {
+        var model = new TSqlModel(SqlServerVersion.Sql130, null);
+
+        model.AddObjects(sql);
+
+        var validationErrors = model.Validate();
+        if (validationErrors.Any())
+        {
+            throw new InvalidOperationException("Warnings or errors present in model");
+        }
+
+        return model;
+    }
+
+    private static TSqlFragment Parse(string sql)
+    {
+        var parser = new TSql130Parser(false, SqlEngineType.Standalone);
+        using var sr = new StringReader(sql);
+
+        return parser.Parse(sr, out _);
+    }
+
+    private static TSqlStatement[] GetStatements<T>(string sql)
+        => GetStatements(sql, typeof(T));
+
+    private static TSqlStatement[] GetStatements(string sql, Type type)
+    {
+        var fragment = (TSqlScript)Parse(sql);
+
+        var statements = fragment.Batches
+            .SelectMany(x => x.Statements)
+            .Where(x => x.GetType() == type)
+            .ToArray();
+
+        return statements;
+    }
+    private static DacPackage GetDacPackage(string sql)
+    {
+        var model = GetTSqlModel(sql);
+
+        var stream = new MemoryStream();
+
+        var packageMetadata = new PackageMetadata()
+        {
+            Name = "MyPackage",
+            Description = "My description",
+            Version = "1.0.0.0"
+        };
+
+        var packageOptions = new PackageOptions
+        {
+            DeploymentContributors = new List<DeploymentContributorInformation>
+            {
+                new DeploymentContributorInformation
+                {
+                    ExtensionId = "Soresu",
+                    Version = Version.Parse("1.0.0.0")
+                }
+            }
+        };
+
+        DacPackageExtensions.BuildPackage(stream, model, packageMetadata, packageOptions);
+
+        var package = DacPackage.Load(stream);
+
+        return package;
     }
 }
